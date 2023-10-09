21374e0d6a0631289e33c594d69a9414ed8f8813
Zelly Snyder <zelly@outlook.com>
2023-10-09 03:06:58 -0700

    create functions to store and retrieve pastebins

---
+++ text_store.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"crypto/sha256"
+	badger "github.com/dgraph-io/badger/v4"
+	"os"
+	"log"
+	"strings"
+	"unicode/utf8"
+	"errors"
+)
+
+type Slug string
+
+const MAX_PASTEBIN_SIZE int = 10 << 20
+
+func openDb() *badger.DB {
+	dbPath := os.Getenv("SO_LIBHACK_PASTE__DB_PATH")
+	if len(dbPath) == 0 {
+		log.Fatal(`Missing environment variable for path of database: "SO_LIBHACK_PASTE__DB_PATH"`)
+	}
+	db, err := badger.Open(badger.DefaultOptions(dbPath))
+	if err != nil {
+		log.Fatal("Fail to open database at \"", dbPath, "\":", err)
+	}
+	return db
+}
+
+func SavePastebin(db *badger.DB, text []byte) (Slug, error) {
+	if utf8.ValidString(string(text)) == false {
+		return "", ErrNotAUtf8String
+	}
+	if len(text) > MAX_PASTEBIN_SIZE {
+		return "", ErrMaxPastebinSizeExceeded
+	}
+	slug := GenerateTextSlug(text)
+	err := db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(slug), text)
+	})
+	return slug, err
+}
+
+func LookupPastebin(db *badger.DB, slug Slug) (string, error) {
+	var textData []byte = nil
+	err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(slug))
+		if err != nil {
+			return err
+		}
+		textData = make([]byte, item.ValueSize())
+		err = item.Value(func(val []byte) error {
+			copy(textData, val)
+			return nil
+		})
+		return err
+	})
+	if err != nil {
+		return "", err
+	}
+	// assume the data is valid if it was persisted to begin with
+	text := string(textData)
+	return text, err
+}
+
+/// GenerateTextSlug uniquely encodes a body of bytes into a short string identifier.
+func GenerateTextSlug(text []byte) Slug {
+	h := sha256.New()
+	h.Write(text)
+	sum := h.Sum(nil)
+	return makeSlug(sum)
+}
+
+/// makeSlug encodes arbitrary bytes to a short string.
+func makeSlug(digest []byte) Slug {
+	var availableCharacters []rune = []rune("acdefghjklmnpqrstuvwxyz023456789")
+	const slugLength int = 8
+	var outputString strings.Builder
+	var count int = 0
+	for _, b := range digest {
+		if count >= slugLength {
+			break
+		}
+		choice := int(b) % len(availableCharacters)
+		outputString.WriteRune(availableCharacters[choice])
+		count++
+	}
+	return Slug(outputString.String())
+}
+
+var (
+	ErrNotAUtf8String = errors.New("Not a UTF-8 string")
+	ErrMaxPastebinSizeExceeded = errors.New("Pastebin text too long")
+)
---
+++ text_store_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+	badger "github.com/dgraph-io/badger/v4"
+)
+
+func TestMakeSlug(t *testing.T) {
+	helloWorldString := "hello world"
+	someData := []byte(helloWorldString)
+	slug := GenerateTextSlug(someData)
+	// produces some output
+	if len(slug) == 0 {
+		t.Fatalf(`Slug generated from "%s" is empty ("%s")`, helloWorldString, slug);
+	}
+	// produces the same output repeatedly
+	slug2 := GenerateTextSlug([]byte(helloWorldString))
+	if len(slug2) == 0 {
+		t.Fatalf(`Slug generated from "%s" is empty ("%s")`, helloWorldString, slug2);
+	}
+	if string(slug) != string(slug2) {
+		t.Fatalf(`First invocation generated "%s" yet second invocation on the same data produced "%s"`, slug2, slug)
+	}
+}
+
+func FuzzMakeSlug(f *testing.F) {
+	f.Fuzz(func(t *testing.T, someString string) {
+		slug := GenerateTextSlug([]byte(someString))
+		if len(slug) == 0 {
+			t.Fatalf(`Slug generated from "%s" is empty ("%s")`, someString, slug)
+		}
+	})
+}
+
+func TestLookupSavePastebins(t *testing.T) {
+	const examplePaste string = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+`
+	db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+	if err != nil {
+		t.Fatalf("Failure to create Badger in-memory database for testing: %v", err)
+	}
+	defer db.Close()
+	slug, err := SavePastebin(db, []byte(examplePaste))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(slug) == 0 {
+		t.Fatalf(`Slug generated from "%s" is empty: %s`, examplePaste, slug)
+	}
+	retrievedPaste, err := LookupPastebin(db, slug)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if retrievedPaste != examplePaste {
+		t.Fatalf(`Expected: "%s", Got: "%s"`, examplePaste, retrievedPaste)
+	}
+}
